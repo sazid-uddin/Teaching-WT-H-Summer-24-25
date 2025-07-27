340e2e68c5d957282dd5c663714fa1a1c13d2352
Md. Sazid Uddin <sazid.uddin@aiub.edu>
2025-07-27 12:20:14 +0600

    added forms, css demos and js demos

---
+++ myScript.js
@@ -0,0 +1,20 @@
+console.log("External JS");
+console.log("Hello world");
+
+var name = "AIUB";
+var what;
+
+var student = {
+   name: "abc",
+   id: "123"
+};
+
+var x = 0.2 + 0.1;
+console.log(x);
+
+var str = 'My name is "jon"';
+console.log(str);
+
+var y = null;
+
+alert('message');
